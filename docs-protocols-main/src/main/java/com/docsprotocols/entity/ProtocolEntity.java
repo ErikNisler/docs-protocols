e01fd503f15ca27b58676000f017b39b8912f7bc
Erik Nisler <erik.nisler@seznam.cz>
2025-05-26 21:05:25 +0200

    COMMIT_4

--- docs-protocols-main/src/main/java/com/docsprotocols/entity/ProtocolEntity.java
+++ docs-protocols-main/src/main/java/com/docsprotocols/entity/ProtocolEntity.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class ProtocolEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
